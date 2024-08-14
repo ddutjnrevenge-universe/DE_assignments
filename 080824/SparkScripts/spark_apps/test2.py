b6dd870e90fc8291e32d24fa6409afc037a93e4a
ddutjnrevenge-universe <hanhnguyen210704@gmail.com>
2024-08-14 08:56:31 +0700

    trying new config for spark job

--- 080824/SparkScripts/spark_apps/test2.py
+++ 080824/SparkScripts/spark_apps/test2.py
@@ -5,13 +5,16 @@ import csv
 import os
 
 # Configuration for Spark
-conf = SparkConf().set("spark.executor.cores", "2")\
-                  .set("spark.executor.memory", "2g")
-
-sc = SparkContext(appName="Test", conf=conf)
+# conf = SparkConf().set("spark.executor.cores", "2")\
+                #   .set("spark.executor.memory", "2g")
+conf = SparkConf().set("spark.executor.memory", "2g")\
+                  .set("spark.executor.cores", "2")
+# conf = SparkConf().set("spark.executor.memory", "2g").set("spark.executor.cores", "2")      
+# sc = SparkContext(appName="Test", conf=conf)
 
 n_workers = 3  # Number of workers
-partition_multiples = range(18, 22, 3)  # Range of partition multiples (3, 6, 9, 12, 15)
+
+partition_multiples = range(18, 19, 6)  # Range of partition multiples (3, 6, 9, 12, 15)
 
 from math import sqrt, ceil
 
@@ -24,13 +27,13 @@ def is_prime(n):
     return True
 
 # Path to the numbers file and output path
-# numbers_file = "/opt/spark/data/numbers.txt"
-numbers_file = "/opt/spark/data/numbers_2.txt"
-# prime_numbers_rdd_output_path = "/opt/spark/data/results/prime_numbers_rdd.txt"
-prime_numbers_rdd_output_path = "/opt/spark/data/results/prime_numbers_rdd_dataset2.txt"
+numbers_file = "/opt/spark/data/numbers_large.txt"
+# numbers_file = "/opt/spark/data/numbers_2.txt"
+prime_numbers_rdd_output_path = "/opt/spark/data/results/prime_numbers_rdd_large.txt"
+# prime_numbers_rdd_output_path = "/opt/spark/data/results/prime_numbers_rdd_dataset2.txt"
 # Path to the CSV file for logging
-csv_file_path = "/opt/spark/data/results/execution_statistics_dataset2.csv"
-
+# csv_file_path = "/opt/spark/data/results/execution_statistics_dataset2.csv"
+csv_file_path = "/opt/spark/data/results/execution_statistics_large.csv"
 # Check if CSV file exists, if not, write the header
 file_exists = os.path.isfile(csv_file_path)
 with open(csv_file_path, mode='a', newline='') as file:
@@ -39,9 +42,12 @@ with open(csv_file_path, mode='a', newline='') as file:
         writer.writerow(["Execution Time (s)", "Number of Workers", "Number of Executors", "Number of Partitions", "Executor Cores", "Executor Memory"])
 
     for num_partitions in partition_multiples:
+        # sc = SparkContext(appName="Test", conf=conf)
+        # app name based on number of partitions
+        sc = SparkContext(appName=f"Test_{num_partitions}_partitions", conf=conf)
         # Start the timer
         start_time = time.time()
-
+        # sc = SparkContext(appName="Test", conf=conf)
         # Load the numbers and process them
         numbers_rdd = sc.textFile(numbers_file)
         numbers_rdd = numbers_rdd.repartition(num_partitions)  # Set the number of partitions
@@ -53,7 +59,8 @@ with open(csv_file_path, mode='a', newline='') as file:
         end_time = time.time()
         interval = end_time - start_time
         print(f"Execution time with {num_partitions} partitions: {interval} seconds")
-
+        
+        
         # Check the number of executors
         sc_java = sc._jsc.sc()
         n_executors = len([executor.host() for executor in sc_java.statusTracker().getExecutorInfos()]) - 1
@@ -63,5 +70,6 @@ with open(csv_file_path, mode='a', newline='') as file:
 
         # Write the statistics to the CSV file
         writer.writerow([interval, n_workers, n_executors, num_partitions, conf.get("spark.executor.cores"), conf.get("spark.executor.memory")])
-
+        # Stop the Spark context
+        sc.stop()
 print(f"Statistics saved to {csv_file_path}")
